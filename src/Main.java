b25f2d7332dc0714c18d00d48da7042f4c660fda
joyce <joycemoraes608@gmail.com>
2024-01-27 13:11:33 +0000

    main

---
+++ src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        System.out.println(">>>>>>listagem dos clientes");
+        System.out.println("________________________");
+        String nome = "fulano";
+        String data ="1980";
+        System.out.println("Nome: "+nome);
+        System.out.println("data de nascimento"+data);
+        System.out.println("_______________________");
+        String nome1 = "ciclano";
+        String data1 ="2000";
+        System.out.println("Nome: " + nome1);
+        System.out.println(" data de nascimento"+data1);
+        System.out.println("_______________________");
+        System.out.println(">>>>>>listagem dos produtos");
+        System.out.println("_______________________");
+        String produto1 = "livro";
+        String preco1 = "R$49.95";
+        System.out.println("nome:" + produto1);
+        System.out.println("preço:" +preco1);
+
+        System.out.println("_______________________");
+        String produto = "celular";
+        String preco = " R$ 1470.76";
+        System.out.println("nome:" + produto);
+        System.out.println("preço:" + preco);
+
+
+
+
+
+
+
+    }
+}
